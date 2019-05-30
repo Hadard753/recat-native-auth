8692ce367c90ece4115ee1a5a84bedd99e51b20c
hadard753 <hadar753@gmail.com>
2019-05-30 10:04:36 +0300

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
+import { Header } from './components/common/Header';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,10 +23,13 @@ interface Props {}
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View>
+        <Header headerText="AppName"/>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.tsx</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
       </View>
     );
   }
