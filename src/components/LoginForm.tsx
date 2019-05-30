0a5530aaf457ca9fb37e3009c8069f28c9fad94e
hadard753 <hadar753@gmail.com>
2019-05-30 13:09:24 +0300

    Login form works !

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -3,11 +3,11 @@ import { Text, StyleSheet } from 'react-native';
 import { Button, Card, CardSection, Field, Spinner } from './common';
 
 export interface LoginFormState {
-    email: string, 
-    password: string, 
-    error: string, 
-    loading: boolean
-};
+  email: string;
+  password: string;
+  error: string;
+  loading: boolean;
+}
 
 class LoginForm extends Component<{}, LoginFormState> {
   state = { email: '', password: '', error: '', loading: false };
@@ -16,10 +16,25 @@ class LoginForm extends Component<{}, LoginFormState> {
     const { email, password } = this.state;
 
     this.setState({ error: '', loading: true });
+    fetch('http://192.168.86.165:3000/api/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ email: email, password: password })
+    })
+      .then(this.handleLoginResponse.bind(this))
+      .catch(this.onLoginFail.bind(this));
+  }
+
+  handleLoginResponse(response: Response) {
+    if (response.ok) this.onLoginSuccess();
+    else throw '';
   }
 
   onLoginFail() {
-    this.setState({ error: 'Authentication Failed', loading: false });
+    this.setState({ error: 'Authentication Failed', loading: false, password: '' });
   }
 
   onLoginSuccess() {
@@ -36,11 +51,7 @@ class LoginForm extends Component<{}, LoginFormState> {
       return <Spinner size="small" />;
     }
 
-    return (
-      <Button onPress={this.onButtonPress.bind(this)}>
-        Log in
-      </Button>
-    );
+    return <Button onPress={this.onButtonPress.bind(this)}>Log in</Button>;
   }
 
   render() {
@@ -65,13 +76,9 @@ class LoginForm extends Component<{}, LoginFormState> {
           />
         </CardSection>
 
-        <Text style={styles.errorTextStyle}>
-          {this.state.error}
-        </Text>
+        <Text style={styles.errorTextStyle}>{this.state.error}</Text>
 
-        <CardSection>
-          {this.renderButton()}
-        </CardSection>
+        <CardSection>{this.renderButton()}</CardSection>
       </Card>
     );
   }
