7cd8bc6e9ba259e9c9f6aabb15bccc39222286f1
hadard753 <hadar753@gmail.com>
2019-06-02 11:59:24 +0300

    saving point

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -1,4 +1,6 @@
+import axios, { AxiosResponse } from 'axios';
 import { AuthActions } from '../models/Actions.model';
+
 
 export const selectCard = ( cardId: number ) => {
     return {
@@ -24,19 +26,28 @@ export const passwordChanged = ( text: string ) => {
 
 export const loginUser = ( email: string, password: string ) => {
     return (dispatch) => {
-        //TODO : take server url out to configuration file and change it to your ip address 
-        fetch('http://192.168.86.190:3000/api/login', {
-        method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ email: email, password: password })
-        })
-        .then((response: Response) => {
-            debugger;
-            dispatch({type: AuthActions.LOGIN_USER_SUCCESS, payload: response.json()})
-        })
-        .catch((err: Error) => dispatch({type: AuthActions.LOGIN_USER_FAIL, payload: err.message}));
+        axios.post('http://192.168.86.190:3000/api/login', { email, password })
+            .then((response: AxiosResponse) => {
+                debugger;
+                if(response.data.ok)
+                    dispatch({type: AuthActions.LOGIN_USER_SUCCESS, payload: response.data.user})
+            })
+            .catch((err: Error) => dispatch({type: AuthActions.LOGIN_USER_FAIL, payload: err.message}));
+
+        // //TODO : take server url out to configuration file and change it to your ip address 
+        // fetch('http://192.168.86.190:3000/api/login', {
+        // method: 'POST',
+        // headers: {
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json'
+        // },
+        // body: JSON.stringify({ email: email, password: password })
+        // })
+        // .then((response) => {
+        //     debugger;
+        //     if(response.ok)
+        //         dispatch({type: AuthActions.LOGIN_USER_SUCCESS, payload: response.json()})
+        // })
+        // .catch((err: Error) => dispatch({type: AuthActions.LOGIN_USER_FAIL, payload: err.message}));
     };
 };
